290d422d2b9d2e4ff85b911af74188bb606bcf88
MatheusNep <matheussaboia@hotmail.com>
2019-02-09 11:56:52 -0200

    Implemented the sharing of images with the database

--- ParseStarterProject/src/main/java/com/parse/starter/activity/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/MainActivity.java
@@ -9,7 +9,10 @@
 package com.parse.starter.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
@@ -21,12 +24,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.LogInCallback;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -36,6 +41,10 @@ import com.parse.starter.R;
 import com.parse.starter.adapter.TabsAdapter;
 import com.parse.starter.useful.SlidingTabLayout;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 
@@ -86,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_settings:
                 return true;
             case R.id.action_share:
+                sharePhotos();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -93,8 +103,63 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
-
     }
+    private void sharePhotos(){
+
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //retorno dos dados selecionados
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null ){
+
+            //recuperar local dos arquivos
+            Uri localImageSelected = data.getData();
+
+            //recuperar a imagem do local selecionado
+            try {
+                Bitmap image = MediaStore.Images.Media.getBitmap(getContentResolver(),localImageSelected);
+
+                //comprimir a imagem selecionada no formato PNG
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                image.compress(Bitmap.CompressFormat.PNG, 75, stream);
+
+                //criar array de bytes da imagem
+                byte[] byteArray = stream.toByteArray();
+
+                //criar um arquivo no formato proprio do parse
+                SimpleDateFormat dateFormat = new SimpleDateFormat("ddmmaaaahhmmss");
+                String imageName = dateFormat.format(new Date());
+                ParseFile parseFile = new ParseFile( imageName +"image.png", byteArray);
+
+                ParseObject parseObject = new ParseObject("image");
+                parseObject.put("username", ParseUser.getCurrentUser().getUsername());
+                parseObject.put("image", parseFile);
+
+                //salvar dados
+                parseObject.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e==null){
+                            Toast.makeText(MainActivity.this,"Your image is shared", Toast.LENGTH_LONG).show();
+
+                        }else {
+                            Toast.makeText(MainActivity.this,"Error in share image - try again", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+        }
+    }
+
     private void logoutUser(){
         ParseUser.logOut();
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
